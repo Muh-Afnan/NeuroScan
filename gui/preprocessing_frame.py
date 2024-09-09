a5bddea2ed7f2c9a03d0b8066ed7fc9495ef1941
Muhammad Afnan <muhammadafnan108@gmail.com>
2024-09-09 18:48:05 +0500

    progress bar

--- gui/preprocessing_frame.py
+++ gui/preprocessing_frame.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from Implementation.preprocessing_data import resize_data,normalize_data
 
 class PreprocessingFrame(tk.Frame):
@@ -16,14 +17,15 @@ class PreprocessingFrame(tk.Frame):
         self.second_window.title("Data Preprocessing")
         self.second_window.geometry("800x500")
         
-        self.progress_bar = tk
+        self.progress_bar = ttk.Progressbar(self.second_window, orient="horizontal", length = 200, mode="indeterminate", variable=progress_var)
+        
         self.inner_frame = tk.Frame(self.second_window)
         self.inner_frame.pack()
 
-        button_style_large = {"font": ("Arial", 14), "width": 15, "height": 2, "padx": 10, "pady": 10}
+        # button_style_large = {"font": ("Arial", 14), "width": 15, "height": 2, "padx": 10, "pady": 10}
 
-        self.resize_button = tk.Button(self.inner_frame, text="Resize Data Set", **button_style_large, command = self.resize_data)
-        self.resize_button.grid(row=0, column=0,padx=10, pady=10)
+        # self.resize_button = tk.Button(self.inner_frame, text="Resize Data Set", **button_style_large, command = self.resize_data)
+        # self.resize_button.grid(row=0, column=0,padx=10, pady=10)
 
-        self.normalize_data = tk.Button(self.inner_frame, text="Normalize Data Set", **button_style_large, command = self.normalize_data)
-        self.normalize_data.grid(row=0, column=1,padx=10, pady=10)
+        # self.normalize_data = tk.Button(self.inner_frame, text="Normalize Data Set", **button_style_large, command = self.normalize_data)
+        # self.normalize_data.grid(row=0, column=1,padx=10, pady=10)
